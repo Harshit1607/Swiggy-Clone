984ea30fa2bdd7db42ee88aac32b263a9de70711
Harshit Bareja <hbareja.07@gmail.com>
2024-07-23 00:44:02 +0530

    started swiggy clone

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='Navbar'>
+        <div className='Navbar-Left'></div>
+        <div className="Navbar-Right"></div>
+    </div>
+  )
+}
+
+export default Navbar
