b49290fc057f0d229f95fc97738f388f56d32291
Harshit Bareja <hbareja.07@gmail.com>
2024-07-24 02:21:43 +0530

    added restaurants data and did basic styling

--- frontend/src/components/Home/Main.jsx
+++ frontend/src/components/Home/Main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Recomendation from './Recomendation'
+import Restaurants from './Restaurants'
+
 
 const Main = () => {
   return (
     <div className="main">
       <Recomendation />
+      <Restaurants />
     </div>
   )
 }
---
+++ frontend/src/components/Home/Restaurants.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import restaurants from '../../assets/restaurants.json'
+
+const Restaurants = () => {
+  return (
+    <div className='restaurants'>
+      <div className="restaurant-heading">
+        <span>Restaurants with online food delivery in Delhi</span>
+      </div>
+      <div className="restaurant-container">
+        { restaurants.map(item=>{
+          return(
+            <div className="restaurant-info">
+              <img src={item.image} />
+              <span className='restaurant-name'>{item.name}</span>
+              <span className='restaurant-rating'>{item.rating}</span>
+              <span className='restaurant-cusine'>{item.cuisine}</span>
+            </div>
+          )
+        })
+        
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Restaurants
